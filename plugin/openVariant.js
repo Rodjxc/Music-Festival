4bb5b4461b02c254b362c56577362df3a3ede727
Rod <rodrysxc@gmail.com>
2023-05-11 18:51:42 +0200

    Hero section finished. Headliner section almost finised

--- plugin/openVariant.js
+++ plugin/openVariant.js
@@ -3,6 +3,11 @@ const plugin = require("tailwindcss/plugin");
 const openVariant = plugin(function ({ addVariant }) {
   addVariant("group-open", ":merge(.group).open &");
   addVariant("peer-open", ":merge(.peer).open ~ & ");
+
+  //We're creating a personalised class called group-open and peer-open witn a new .open class
+  //for the peer we're using the sibling selector with the ~ since we're gonna use the sibling
+  //element of the peer class
+
 });
 
 module.exports = openVariant;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
-const { default: plugin } = require('tailwindcss');
+const { default: plugin } = require("tailwindcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [require("./plugin/openVariant" )],
-};
+  theme: {
+    //we're "extending" the things TailwindCSS has with a custom animation
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+
+        //The above is "an extension" that we customised for the bars to move
+        //up and down at different speeds. So at 0% it will start the animation
+        //at 0.5 of their original height, and at 50% they'll do 3x their height
+        //to go back over to the 0.5.
+      },
+    },
+    animation: {
+      wavey: "wavey 1000ms linear infinite",
+      // we take the "wavey" keyframe, and make it infinite in an animation
+      //it'll implement a class called animate-wavey
+    },
+  },
+
+  //
+  plugins: [require("./plugin/openVariant"), require("./plugin/animationDelay")]};
